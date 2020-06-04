ce9e2abd3485c5642d5fd4a4375c982c01c1da59
Ajay Dabas <se.dabasajay@gmail.com>
2020-06-05 04:36:50 +0530

    finished initial version of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,71 @@
-import React from "react";
-import './App.css';
+import React, { useEffect } from "react";
 
-const App = (props: Object) : JSX.Element => {
+import { ApplicationContainer } from './providers/application';
+
+import Dashboard from './views/dashboard/dashboard';
+import LoadingSpinner from './views/spinner/spinner';
+
+export const App = () : JSX.Element => {
+
+  const {
+    state: applicationState,
+    setApplicationLoaded,
+    pushToDo,
+    populateStateFromLocalStorage: populate,
+    isStateEmpty
+  } = ApplicationContainer.useContainer();
+
+  const sleep = (millisec: number) : Promise<void> => {
+    return new Promise(resolve => setTimeout(resolve, millisec));
+  }
+
+  useEffect(() => {
+    const work = async () => {
+      // Upon loading app, populate todos data from local storage
+      populate();
+      // Wait 3 second to let hooks update app state (if found)
+      await sleep(3000);
+      // If it's still empty, fetch from Chuck Norris jokes API
+      if(isStateEmpty()){
+        const initialTodos : string[] = [];
+        for(let i = 1; i <= 3; i++){
+          const response = await fetch("https://api.chucknorris.io/jokes/random?category=dev");
+          const result = await response.json();
+          const { value } = result;
+          initialTodos.push(value);
+        }
+        pushToDo(initialTodos);
+        // Wait 3 second to let hooks update app state
+        await sleep(3000);
+      }
+      // Load application
+      setApplicationLoaded();
+    }
+    work();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <div className = 'App' data-test = 'component-app'>
-      <div>Add a Todo</div>
-      <hr className = 'hline'/>
-      <div style = {{textAlign: 'center'}}>
-        <form>
-          <textarea className = "form-textarea" placeholder = {'Let\'s do it...'} rows = {5}></textarea>
-          <br/>
-          <button type = "submit" className = "btn">Add</button>
-        </form>
-      </div>
-      <div>Todos</div>
-      <hr className = 'hline'/>
-    </div>
+    applicationState.isLoaded
+    ?
+    (
+      <Dashboard/>
+    )
+    :
+    (
+      <LoadingSpinner/>
+    )
   );
 }
 
-export default App;
+type ProviderWrapperPropTypes = {
+  children: JSX.Element
+};
+
+export const ProviderWrapper = (props : ProviderWrapperPropTypes) : JSX.Element => {
+  return (
+    <ApplicationContainer.Provider>
+      {props.children}
+    </ApplicationContainer.Provider>
+  );
+}
