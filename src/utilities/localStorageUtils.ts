74d1145e1a7f6f67534b55c3b2678b77776e2f88
Ajay Dabas <se.dabasajay@gmail.com>
2020-06-05 16:44:18 +0530

    fixed bug to discard local data is todoType changes and keys are not found

--- src/utilities/localStorageUtils.ts
+++ src/utilities/localStorageUtils.ts
@@ -1,11 +1,26 @@
 import { todoType } from '../providers/application';
 
+const allFieldsPresent = (todo : todoType) : boolean => {
+  const sampleObject : todoType = {
+    id: 0,
+    status: false,
+    text: ''
+  };
+  const sampleKeys : Array<string> = Object.keys(sampleObject).sort();
+  const actualKeys : Array<string> = Object.keys(todo).sort();
+  return JSON.stringify(sampleKeys) === JSON.stringify(actualKeys);
+}
+
 export const getTodosFromLocalStorage = () : todoType[] => {
   // Parse the stored object in local storage and return data
   let todosStringified : string | null = localStorage.getItem('todos');
   if(!todosStringified)
     todosStringified = '[]';
   const todosParsed : todoType[] = JSON.parse(todosStringified);
+  // Make sure the if todoType type is changed, previous local storage
+  // data is discarded.
+  if(todosParsed.length > 0 && !allFieldsPresent(todosParsed[0]))
+    return [];
   return todosParsed;
 }
 
