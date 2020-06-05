3d08cb9fa60c1e7fcfda55b4643ec5a5f4d4be60
Ajay Dabas <se.dabasajay@gmail.com>
2020-06-05 19:27:46 +0530

    added active and completed todo count feature

--- src/views/dashboard/dashboard.tsx
+++ src/views/dashboard/dashboard.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './dashboard.css';
+
+import { ApplicationContainer } from '../../providers/application';
+
 import { HLine } from '../../components/hline/hline';
 
 import { Writer } from '../writer/writer';
@@ -7,6 +10,19 @@ import { Editor } from '../editor/editor';
 import { Viewer } from '../viewer/viewer';
 
 const Dashboard = () : JSX.Element => {
+
+  const {
+    state: applicationState
+  } = ApplicationContainer.useContainer();
+
+  const activeCount = applicationState.todos.filter(
+    (item) => item.status === true
+  ).length;
+
+  const completedCount = applicationState.todos.filter(
+    (item) => item.status === false
+  ).length;
+
   return (
     <div className = 'Dashboard'>
       {/* Heading */}
@@ -18,14 +34,14 @@ const Dashboard = () : JSX.Element => {
       <Writer/>
       {/* Heading */}
       <div>
-        Active To-Dos
+        Active To-Dos <span style = {{color: '#005C42'}}>({activeCount})</span>
       </div>
       <HLine/>
       {/* Editor - View, Edit, Delete, Mark as complete a todo */}
       <Editor/>
       {/* Heading */}
       <div>
-        Completed To-Dos
+        Completed To-Dos <span style = {{color: '#821517'}}>({completedCount})</span>
       </div>
       <HLine/>
       {/* Viewer - View, Delete, Mark as incomplete a todo */}
