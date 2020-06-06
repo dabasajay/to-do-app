5adaa6940995871750ebbc900f45d2a640c5707d
Ajay Dabas <se.dabasajay@gmail.com>
2020-06-06 23:51:08 +0530

    fixed some code indentation

--- src/components/textArea/textArea.tsx
+++ src/components/textArea/textArea.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import './textArea.css';
 
 export type TextAreaPropTypes = {
-	placeholder?: string,
-	rows?: number,
-	onChange?: (event: any) => void,
-	readOnly?: boolean,
-	className?: string,
-	style?: object,
-	initialValue?: string,
-	reference?: any
+  placeholder?: string,
+  rows?: number,
+  onChange?: (event: any) => void,
+  readOnly?: boolean,
+  className?: string,
+  style?: object,
+  initialValue?: string,
+  reference?: any
 };
 
 export const TextArea = (props: TextAreaPropTypes) : JSX.Element => {
-	return (
+  return (
     <textarea
-			className = {`form-textarea ${props.className ? props.className : ''}`}
-			style = {props.style ? props.style : {}}
-			placeholder = {props.placeholder ? props.placeholder : ''}
-			rows = {props.rows ? props.rows : 3}
-			onChange = {props.onChange}
-			readOnly = {props.readOnly ? props.readOnly : false}
-			value = {props.initialValue ? props.initialValue : ''}
-			ref = {props.reference ? props.reference : null}
-			data-test = 'component-textarea'
-		>
+      className = {`form-textarea ${props.className ? props.className : ''}`}
+      style = {props.style ? props.style : {}}
+      placeholder = {props.placeholder ? props.placeholder : ''}
+      rows = {props.rows ? props.rows : 3}
+      onChange = {props.onChange}
+      readOnly = {props.readOnly ? props.readOnly : false}
+      value = {props.initialValue ? props.initialValue : ''}
+      ref = {props.reference ? props.reference : null}
+      data-test = 'component-textarea'
+    >
     </textarea>
-	);
+  );
 }
