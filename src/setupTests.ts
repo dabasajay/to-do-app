4044fcefa3ee64535f2af73857739dce50f1c288
Ajay Dabas <se.dabasajay@gmail.com>
2020-06-06 20:15:43 +0530

    code for mocking local storage

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -10,3 +10,27 @@ import EnzymeAdapter from 'enzyme-adapter-react-16';
 // Configure Enzyme
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
+
+// Setup mock local storage
+
+let localStorageMock = (function(){
+  let store : {
+    [index: string]: string
+  } = {};
+  return {
+    getItem: function(key : string){
+      return store[key];
+    },
+    setItem: function(key : string, value : string){
+      store[key] = value.toString();
+    },
+    clear: function() {
+      store = {};
+    },
+    removeItem: function(key: string){
+      delete store[key];
+    }
+  };
+});
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock() });
